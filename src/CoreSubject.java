ee4a311da45d19d9d95576011dc9009527e5c0b8
Harriet Effah <harriet.effah@amalitech.com>
2025-12-09 07:05:52 +0000

    Revised comments in CoreSubject class and ElectiveSubject class

--- src/CoreSubject.java
+++ src/CoreSubject.java
@@ -1,23 +1,26 @@
-public class CoreSubject extends Subject{
+public class CoreSubject extends Subject{//the CoreSubject class inherits from Subject
+    //private field specific to CoreSubject
     private final boolean mandatory = true;//value cannot be changed, set to true because core subjects are mandatory
 
+     //This constructor calls the parent constructor(from Subject) using the 'super' keyword
     public CoreSubject(String subjectName, String subjectCode){
         super(subjectName, subjectCode);
     }
 
-    @Override
+    @Override  // overriding the abstract method, displaySubjectDetails from Subject class
     public void displaySubjectDetails() {
 
+        //Printing out the subject information
         System.out.println("Core Subject: " + getSubjectName() + "(" + getSubjectCode() + ") - Mandatory: " +  mandatory);
 
     }
 
-    @Override
+    @Override // overriding the abstract method, getSubjectType to return 'Core'
     public String getSubjectType() {
         return "Core";
     }
-
+    //This method checks if the subject is mandatory
     public boolean isMandatory() { // this method checks if the subject is mandatory
         return true;
-    }
+    } // must return true because core subjects are mandatory
 }
--- src/ElectiveSubject.java
+++ src/ElectiveSubject.java
@@ -1,21 +1,24 @@
-public class ElectiveSubject extends Subject {
-    private final boolean mandatory = false;
+public class ElectiveSubject extends Subject { // inherits from the Subject class and represents optional subjects
+    private  boolean mandatory = false;// always false for elective subjects
 
+    //This constructor calls the parent constructor using the 'super' keyword
     public ElectiveSubject(String subjectName, String subjectCode) {
         super(subjectName, subjectCode);
+        this.mandatory = false; //elective subjects are not mandatory
     }
 
-    @Override
+    @Override // overriding the abstract method displaySubjectDetails from the Subject class
     public void displaySubjectDetails() {
         System.out.println("Elective Subject: " + getSubjectName() +
-                " (" + getSubjectCode() + ") - Mandatory: " + mandatory);
+                " (" + getSubjectCode() + ") - Mandatory: " + mandatory);// prints out subject details
     }
 
-    @Override
+    @Override // overriding the  abstract method, getSubjectType to return 'Elective'
     public String getSubjectType() {
         return "Elective";
     }
 
+    //this method checks if elective is mandatory ad returns false
     public boolean isMandatory() {
         return false;
     }

e1a3d4d1c9ddbadc03fdc7b0e4864bff5f04b765
Harriet Effah <harriet.effah@amalitech.com>
2025-12-12 03:33:48 +0000

    feat : added Calculable Interface for adding statistics

---
+++ src/Calculable.java
@@ -0,0 +1,5 @@
+//this interface is for calculating statistics
+interface Calculable {
+    double calculateAverage();              // calculates average
+    double calculateGPA();                  // calculates GPA (4.0 scale)
+}

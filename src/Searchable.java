c7644048a48896f4c9f0bc45aec2b9fbb3027dee
Harriet Effah <harriet.effah@amalitech.com>
2025-12-12 03:31:34 +0000

    feat: added Searchable Interfacts for searching objects

---
+++ src/Searchable.java
@@ -0,0 +1,6 @@
+//this interface is  for objects that can be searched
+interface Searchable {
+    boolean matchesId(String id);          // searching by ID
+    boolean matchesName(String name);       // searching by name (partial)
+    boolean matchesType(String type);       // searching by type
+}
